9a65c5fcf08955da3ca909f44250c2a86aa1bd77
井上大誠 <59153204+taise-hub@users.noreply.github.com>
2024-11-13 00:17:49 +0900

    db定義

--- main.go
+++ main.go
@@ -14,15 +14,45 @@ func main() {
 	}
 	defer db.Close()
 	sqlStmt := `
-    CREATE TABLE IF NOT EXISTS domains (
-	    id TEXT PRIMARY KEY,
-        name TEXT UNIQUE
-    );
-    CREATE TABLE IF NOT EXISTS subdomains (
-	    id TEXT PRIMARY KEY,
-        name TEXT UNIQUE,
-		parent_id TEXT NOT NULL
-    );`
+CREATE TABLE IF NOT EXISTS corps (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS domains (
+    id TEXT PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    corp_id TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (corp_id) REFERENCES corps(id)
+);
+
+CREATE TABLE IF NOT EXISTS subdomains (
+    id TEXT PRIMARY KEY,
+    parent_id TEXT NOT NULL,
+    root_id TEXT NOT NULL,
+    name TEXT NOT NULL,
+    tools_detected TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (parent_id) REFERENCES subdomains(id),
+    FOREIGN KEY (root_id) REFERENCES domains(id)
+);
+
+CREATE TABLE IF NOT EXISTS links (
+    id TEXT PRIMARY KEY,
+    url TEXT NOT NULL,
+    domain_id TEXT,
+    subdomain_id TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (domain_id) REFERENCES domains(id),
+    FOREIGN KEY (subdomain_id) REFERENCES subdomains(id)
+);
+`
 
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
